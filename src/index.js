9b90b9e58c8842033f928e60920245c0279ae32d
Erick Santos <eksantos.jr@gmail.com>
2020-05-24 15:41:23 -0700

    Change toolbox and filter widths

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import drawDropdown from './dropdown';
 import {displaySeq} from './display_seq';
 
 document.addEventListener("DOMContentLoaded", () => {
+    
     document.getElementById("content").style.display = "flex";
     drawDropdown();
     displaySeq();
