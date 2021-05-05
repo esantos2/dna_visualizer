7c4b8b4c4d2ab69d468900641034788cef3a2541
Erick Santos <eksantos.jr@gmail.com>
2021-05-04 20:28:00 -0700

    Refactor modal and button text references

--- src/tutorial_modals/intro_modal.js
+++ src/tutorial_modals/intro_modal.js
@@ -4,8 +4,20 @@ export {
     openTutorial
 };
 
-const PREV_ARROW = "< Prev";
-const NEXT_ARROW = "Next >";
+
+const BUTTON_TEXT = {
+    PREV_ARROW: "< Prev",
+    NEXT_ARROW: "Next >",
+    START: "Get Started"
+};
+
+const MODAL_TEXT = {
+    TITLE: "Welcome to the DNA Visualizer",
+    OVERVIEW: "Explore and analyze DNA sequences from a variety of species and diseases",
+    REGION_SELECT: "Click + Drag to select a region, then click \"Select Region\" to zoom in on your selection",
+    FILTERS: "Highlight different bases using filters",
+    DATA_MODELS: "Bar charts and the 3D model provide more to explore and update with new selections"
+};
 
 const openTutorial = () => {
     const modal = _modalTemplate();
@@ -46,47 +58,47 @@ const addNavigationButton = (buildNextModal, modalContext, buttonTextDirection) 
 const modal1 = (welcomePrompt) => {
     welcomePrompt.innerHTML = "";
     let title = document.createElement("h1");
-    title.innerHTML = "Welcome to the DNA Visualizer"
+    title.innerHTML = MODAL_TEXT.TITLE;
     let desc = document.createElement("p");
-    desc.innerHTML = "Explore and analyze DNA sequences from a variety of species and diseases"
+    desc.innerHTML = MODAL_TEXT.OVERVIEW;
     let newLine = document.createElement("p");
-    newLine.innerHTML = "Click + Drag to select a region, then click \"Select Region\" to zoom in on your selection";
+    newLine.innerHTML = MODAL_TEXT.REGION_SELECT;
     const newImg = createImageElement("dist/gifs/selected_seq4.gif");
     welcomePrompt.appendChild(title);
     welcomePrompt.appendChild(desc);
     welcomePrompt.appendChild(newLine);
     welcomePrompt.appendChild(newImg);
-    addNavigationButton(modal2, welcomePrompt, NEXT_ARROW);
+    addNavigationButton(modal2, welcomePrompt, BUTTON_TEXT.NEXT_ARROW);
 }
 
 const modal2 = (welcomePrompt) => {
     welcomePrompt.innerHTML = "";
     let newLine = document.createElement("p");
-    newLine.innerHTML = "Highlight different bases using filters";
+    newLine.innerHTML = MODAL_TEXT.FILTERS;
     const newImg = createImageElement("dist/gifs/filter3.gif");
     welcomePrompt.appendChild(newLine);
     welcomePrompt.appendChild(newImg);
-    addNavigationButton(modal1, welcomePrompt, PREV_ARROW);
-    addNavigationButton(modal3, welcomePrompt, NEXT_ARROW);
+    addNavigationButton(modal1, welcomePrompt, BUTTON_TEXT.PREV_ARROW);
+    addNavigationButton(modal3, welcomePrompt, BUTTON_TEXT.NEXT_ARROW);
 }
 
 const modal3 = (welcomePrompt) => {
     welcomePrompt.innerHTML = "";
     let newLine = document.createElement("p");
-    newLine.innerHTML = "Bar charts and the 3D model provide more to explore and update with new selections";
+    newLine.innerHTML = MODAL_TEXT.DATA_MODELS;
     const images = createElementWithClass("div", "last-modal-images");
     const newImg1 = createImageElement("dist/gifs/charts1.gif");
     const newImg2 = createImageElement("dist/gifs/strand2.gif");
     images.appendChild(newImg1);
     images.appendChild(newImg2);
     let startBtn = document.createElement("button");
-    startBtn.innerHTML = "Get Started";
+    startBtn.innerHTML = BUTTON_TEXT.START;
     startBtn.addEventListener("click", (e) => {
         e.preventDefault();
         closeModal(e);
     })
     welcomePrompt.appendChild(newLine);
     welcomePrompt.appendChild(images);
-    addNavigationButton(modal2, welcomePrompt, PREV_ARROW);
+    addNavigationButton(modal2, welcomePrompt, BUTTON_TEXT.PREV_ARROW);
     welcomePrompt.appendChild(startBtn);
 }
